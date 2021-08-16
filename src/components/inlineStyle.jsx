ebf7dedea557ffa51d9fcc66e6177bb114dfe1f4
tnishiz <85749872+tnishiz@users.noreply.github.com>
2021-08-16 10:28:03 +0900

    演習12

--- src/components/inlineStyle.jsx
+++ src/components/inlineStyle.jsx
@@ -1,8 +1,27 @@
 export const InlineStyle = () => {
+  const cotainerStyle = {
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    margin: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  };
+  const titleStyle = {
+    margin: 0,
+    color: "#3d84a8"
+  };
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
   return (
-    <div>
-      <p>- Inline Styles-</p>
-      <button>FIGHT!!</button>
+    <div style={cotainerStyle}>
+      <p style={titleStyle}>- Inline Styles-</p>
+      <button style={buttonStyle}>FIGHT!!</button>
     </div>
   );
 };
